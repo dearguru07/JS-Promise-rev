8e70434f9d831ee870e1cd0728ab3549b5fd6f25
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-29 20:23:36 +0530

    'sw'

--- app.js
+++ app.js
@@ -31,22 +31,32 @@
   
 
 
-function Download(){
-    console.log('btn clicked...')
-    console.log('feacthing the server')
-    let myPromise=new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log('downloading the movie')
-        resolve()
-        },5000)
-    })
-    myPromise.then(()=>{
-        console.log('movoe downloaded')
-        console.log('movie storaded ur mbl')
-    }).catch(()=>{
-        console.log('server is broken')
-    }).finally(()=>{
-        console.log('fianally movie is watching')
-    })
+// function Download(){
+//     console.log('btn clicked...')
+//     console.log('feacthing the server')
+//     let myPromise=new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             console.log('downloading the movie')
+//         resolve()
+//         },5000)
+//     })
+//     myPromise.then(()=>{
+//         console.log('movoe downloaded')
+//         console.log('movie storaded ur mbl')
+//     }).catch(()=>{
+//         console.log('server is broken')
+//     }).finally(()=>{
+//         console.log('fianally movie is watching')
+//     })
 
+// }
+
+
+function Download(){
+   let data= fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0445612&lng=77.57268839999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+//    console.log(data)
+    .then((data)=>{
+        let originaData=data.json()
+        console.log(originaData)
+    })
 }
