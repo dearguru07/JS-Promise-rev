8ecd310eaa3542b9f2c170fa68a39ed8ac7ec48d
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-29 19:22:33 +0530

    'pr'

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("one");
+console.log("two");
+
+
+
+let myPromise = new Promise((resolve, reject) => {
+  resolve();
+  console.log("three");
+  console.log("four");
+  console.lg("five");
+});
+myPromise.then(()=>{
+    console.log("six");
+    console.log("seven");
+}).finally(()=>{
+    console.log("eight");
+})
