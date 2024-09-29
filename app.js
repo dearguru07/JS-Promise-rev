a88cc776da2935e70c3ab04366812b80617549c4
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-29 19:24:46 +0530

    'b'

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 console.log("one");
 console.log("two");
 
+// let myPromise = new Promise((resolve, reject) => {
+//   resolve();
+//   console.log("three");
+//   console.log("four");
+//   console.lg("five");
+// });
+// myPromise.then(()=>{
+//     console.log("six");
+//     console.log("seven");
+// }).finally(()=>{
+//     console.log("eight");
+// })
 
 
-let myPromise = new Promise((resolve, reject) => {
-  resolve();
-  console.log("three");
-  console.log("four");
-  console.lg("five");
-});
-myPromise.then(()=>{
-    console.log("six");
-    console.log("seven");
-}).finally(()=>{
-    console.log("eight");
-})
+
+// let myPromise = new Promise((resolve, reject) => {
+//     reject();
+//     console.log("three");
+//     console.log("four");
+//     console.lg("five");
+//   });
+//   myPromise.then(()=>{
+//       console.log("six");
+//       console.log("seven");
+//   }).finally(()=>{
+//       console.log("eight");
+//   })
+  
+
