1e5692fa5b2bcf557abc0a42e259f86633f92826
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-30 15:41:28 +0530

    'py'

---
+++ index.py
@@ -0,0 +1,41 @@
+# print(True and False)
+# print(False and False)
+
+# print(True or False)
+# print(False or False)
+
+
+# print(not True)
+# print(not False)
+
+# print(False or "")
+# print(10 and 20)
+
+# print("" and 20)
+
+
+# and opertator(Non boolean)----------------
+
+# print(20 and 45) # ==> first value is not zero so, o/p will be the sec value
+
+# print(0 and 50) # first value is zero or false , o/p is it self
+
+
+# or opertator (non boolean)------------------
+
+# print(2 or 50)  # ==> first value is not zero so, o/p will be the it self
+
+# print(0 or 45)   # first value is zero or false , o/p will be the sec value
+
+
+# not opertator-------------
+
+# print(not 45)
+
+# print(bin(4))3
+
+# print(bin(133 ))
+
+# print(bin(6))
+
+
